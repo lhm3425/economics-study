7ec8ed44faa0ab1bfb98b30c438477be76a93ebf
LeeHyeonmin <100504041+lhm3425@users.noreply.github.com>
2026-07-05 11:44:05 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bok-concepts-v19";
+const CACHE_NAME = "bok-concepts-v20";
 const ASSETS = [
   "./",
   "./index.html",
@@ -9,7 +9,11 @@ const ASSETS = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache =>
+      Promise.all(ASSETS.map(asset =>
+        fetch(asset, { cache: "reload" }).then(response => cache.put(asset, response))
+      ))
+    )
   );
   self.skipWaiting();
 });
